709431a71b0e4738a23b46094762f21e9a8d2030
FlochNathan <flochnathan@gmail.com>
2026-06-27 13:29:25 +0200

    api stub sans dépendance Express pour démo Vercel

--- api/[...path].js
+++ api/[...path].js
@@ -1,1 +1,5 @@
-module.exports = require('../server/index');
+// Backend non actif en mode démo (REACT_APP_MOCK=true)
+// Ce fichier sera remplacé quand la DB Supabase sera connectée
+module.exports = (req, res) => {
+  res.status(503).json({ error: 'Backend non disponible en mode démo' });
+};
